636ea85b8b6534a14eb0fa1938b43ff638fdac3e
Olivier Wouters <olivier5741@gmail.com>
2026-07-24 15:30:31 +0200

    form control responsive and centered

--- app/service-worker.js
+++ app/service-worker.js
@@ -42,7 +42,7 @@ var precacheConfig = [
   ["favicon-big.png", "2d4b15686dbaf4e246ac1dc9938e2a90"],
   ["favicon.ico", "ee611bbcef485079c74fe05e671dbd1e"],
   ["favicon.png", "e7a6882479fac071ae2048ddf09d2241"],
-  ["index.html", "ab3cc2170996438a9e7fc1aa0c53f8e1"],
+  ["index.html", "462fc9f077b74ef1b53a76a0090d4d4a"],
   ["manifest.json", "d5bb9c72c7ce78e3180e656115466738"],
 ];
 var cacheName =
